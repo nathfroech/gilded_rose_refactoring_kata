ef1ffd3ded523ea77be11c4fd493d1a69df260ca
Timur Daukaev <timurdaukaev@gmail.com>
2020-07-05 17:14:47 +0300

    Refactor GildedRose.update_quality

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Callable, Dict, List
 
 AGED_BRIE = 'Aged Brie'
 SULFURAS = 'Sulfuras, Hand of Ragnaros'
@@ -12,36 +12,44 @@ LEGENDARY_QUALITY = 80
 class GildedRose:
     def __init__(self, items: List['Item']) -> None:
         self.items = items
+        self.special_handlers: Dict[str, Callable[[Item], None]] = {
+            AGED_BRIE: self._update_aged_brie,
+            SULFURAS: self._update_legendary_item,
+            BACKSTAGE_PASS: self._update_backstage_pass,
+        }
 
     def update_quality(self) -> None:
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            handle_item = self.special_handlers.get(item.name, self._update_regular_item)
+            handle_item(item)
+
+    def _update_regular_item(self, item: 'Item') -> None:
+        item.sell_in -= 1
+        if item.sell_in >= 0:
+            item.quality = max(item.quality - 1, MIN_QUALITY)
+        else:
+            item.quality = max(item.quality - 2, MIN_QUALITY)
+
+    def _update_aged_brie(self, item: 'Item') -> None:
+        item.sell_in -= 1
+        if item.sell_in >= 0:
+            item.quality = min(item.quality + 1, MAX_QUALITY)
+        else:
+            item.quality = min(item.quality + 2, MAX_QUALITY)
+
+    def _update_legendary_item(self, item: 'Item') -> None:
+        """Update sell_in and quality for a legendary item (i.e. don't do anything)."""
+
+    def _update_backstage_pass(self, item: 'Item') -> None:
+        item.sell_in -= 1
+        if item.sell_in > 10:
+            item.quality = min(item.quality + 1, MAX_QUALITY)
+        elif item.sell_in > 5:
+            item.quality = min(item.quality + 2, MAX_QUALITY)
+        elif item.sell_in >= 0:
+            item.quality = min(item.quality + 3, MAX_QUALITY)
+        else:
+            item.quality = MIN_QUALITY
 
 
 class Item:
