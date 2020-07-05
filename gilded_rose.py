305d02ddc87bef172f7c7058f9c0a7b8812f2f50
Timur Daukaev <timurdaukaev@gmail.com>
2020-07-05 20:01:25 +0300

    Rewrite with classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List
+from typing import List
 
 AGED_BRIE = 'Aged Brie'
 SULFURAS = 'Sulfuras, Hand of Ragnaros'
@@ -12,63 +12,86 @@ LEGENDARY_QUALITY = 80
 class GildedRose:
     def __init__(self, items: List['Item']) -> None:
         self.items = items
-        self.special_handlers: Dict[str, Callable[[Item], None]] = {
-            AGED_BRIE: self._update_aged_brie,
-            SULFURAS: self._update_legendary_item,
-            BACKSTAGE_PASS: self._update_backstage_pass,
-        }
 
     def update_quality(self) -> None:
         for item in self.items:
-            if item.name in self.special_handlers:
-                handle_item = self.special_handlers[item.name]  # noqa: WPS529
-            elif item.name.startswith('Conjured'):
-                handle_item = self._update_conjured_item
-            else:
-                handle_item = self._update_regular_item
-            handle_item(item)
-
-    def _update_regular_item(self, item: 'Item') -> None:
-        item.sell_in -= 1
-        if item.sell_in >= 0:
-            item.quality = max(item.quality - 1, MIN_QUALITY)
-        else:
-            item.quality = max(item.quality - 2, MIN_QUALITY)
-
-    def _update_aged_brie(self, item: 'Item') -> None:
-        item.sell_in -= 1
-        if item.sell_in >= 0:
-            item.quality = min(item.quality + 1, MAX_QUALITY)
-        else:
-            item.quality = min(item.quality + 2, MAX_QUALITY)
-
-    def _update_legendary_item(self, item: 'Item') -> None:
-        """Update sell_in and quality for a legendary item (i.e. don't do anything)."""
-
-    def _update_backstage_pass(self, item: 'Item') -> None:
-        item.sell_in -= 1
-        if item.sell_in > 10:
-            item.quality = min(item.quality + 1, MAX_QUALITY)
-        elif item.sell_in > 5:
-            item.quality = min(item.quality + 2, MAX_QUALITY)
-        elif item.sell_in >= 0:
-            item.quality = min(item.quality + 3, MAX_QUALITY)
-        else:
-            item.quality = MIN_QUALITY
-
-    def _update_conjured_item(self, item: 'Item') -> None:
-        item.sell_in -= 1
-        if item.sell_in >= 0:
-            item.quality = max(item.quality - 2, MIN_QUALITY)
-        else:
-            item.quality = max(item.quality - 4, MIN_QUALITY)
+            item.update()
 
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __new__(cls, name: str, *args, **kwargs) -> 'Item':
+        klass = Item
+        if name == AGED_BRIE:
+            klass = QualityIncreasingItem
+        elif name == SULFURAS:
+            klass = LegendaryItem
+        elif name == BACKSTAGE_PASS:
+            klass = BackstagePass
+        elif name.startswith('Conjured '):
+            klass = ConjuredItem
+        return object.__new__(klass)  # noqa: WPS609
+
+    def __init__(self, name: str, sell_in: int, quality: int) -> None:
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update(self) -> None:
+        self._update_sell_in()
+        self.quality = self._calculate_new_quality(self.sell_in)
+
+    def _update_sell_in(self) -> None:
+        self.sell_in -= 1
+
+    def _calculate_new_quality(self, new_sell_in: int) -> int:
+        if new_sell_in >= 0:
+            return max(self.quality - 1, MIN_QUALITY)
+        else:
+            return max(self.quality - 2, MIN_QUALITY)
+
+
+class QualityIncreasingItem(Item):
+    def _calculate_new_quality(self, new_sell_in: int) -> int:
+        if new_sell_in >= 0:
+            return min(self.quality + 1, MAX_QUALITY)
+        else:
+            return min(self.quality + 2, MAX_QUALITY)
+
+
+class LegendaryItem(Item):
+    def __init__(self, name: str, sell_in: int, quality: int = LEGENDARY_QUALITY) -> None:
+        super().__init__(name, sell_in, quality)
+        # Parameter is kept for signatures compatibility, but we don't care about the value.
+        self.quality = LEGENDARY_QUALITY
+
+    def _update_sell_in(self) -> None:
+        pass  # noqa: WPS420
+
+    def _calculate_new_quality(self, new_sell_in: int) -> int:
+        return self.quality
+
+
+class BackstagePass(Item):
+    def _calculate_new_quality(self, new_sell_in: int) -> int:
+        if new_sell_in > 10:
+            return min(self.quality + 1, MAX_QUALITY)
+        elif new_sell_in > 5:
+            return min(self.quality + 2, MAX_QUALITY)
+        elif new_sell_in >= 0:
+            return min(self.quality + 3, MAX_QUALITY)
+        else:
+            return MIN_QUALITY
+
+
+class ConjuredItem(Item):
+    def _calculate_new_quality(self, new_sell_in: int) -> int:
+        # From requirements: "Conjured" items degrade in Quality twice as fast as normal items
+        # It would probably be better to bind it somehow to parent method's result - but I'm afraid that this may make
+        # other quality change checks uglier.
+        if new_sell_in >= 0:
+            return max(self.quality - 2, MIN_QUALITY)
+        else:
+            return max(self.quality - 4, MIN_QUALITY)
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,143 +1,152 @@
 import pytest
-from hamcrest import assert_that, contains_exactly, has_properties  # type: ignore
+from hamcrest import assert_that, contains_exactly, equal_to, has_properties, is_  # type: ignore
 
-from gilded_rose import (
-    AGED_BRIE,
-    BACKSTAGE_PASS,
-    LEGENDARY_QUALITY,
-    MAX_QUALITY,
-    MIN_QUALITY,
-    SULFURAS,
-    GildedRose,
-    Item,
-)
+import gilded_rose
 
 
 class TestGildedRose:
+    def test_combined_case(self):
+        items = [
+            gilded_rose.Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
+            gilded_rose.Item(name=gilded_rose.AGED_BRIE, sell_in=2, quality=gilded_rose.MIN_QUALITY),
+            gilded_rose.Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
+            gilded_rose.Item(name=gilded_rose.SULFURAS, sell_in=0, quality=gilded_rose.LEGENDARY_QUALITY),
+            gilded_rose.Item(name=gilded_rose.SULFURAS, sell_in=-1, quality=gilded_rose.LEGENDARY_QUALITY),
+            gilded_rose.Item(name=gilded_rose.BACKSTAGE_PASS, sell_in=15, quality=20),
+            gilded_rose.Item(name=gilded_rose.BACKSTAGE_PASS, sell_in=10, quality=49),
+            gilded_rose.Item(name=gilded_rose.BACKSTAGE_PASS, sell_in=5, quality=49),
+            gilded_rose.Item(name='Conjured Mana Cake', sell_in=3, quality=6),
+        ]
+        gilded_rose_instance = gilded_rose.GildedRose(items)
+
+        gilded_rose_instance.update_quality()
+
+        assert_that(gilded_rose_instance.items, contains_exactly(
+            has_properties(sell_in=9, quality=19),
+            has_properties(sell_in=1, quality=1),
+            has_properties(sell_in=4, quality=6),
+            has_properties(sell_in=0, quality=gilded_rose.LEGENDARY_QUALITY),
+            has_properties(sell_in=-1, quality=gilded_rose.LEGENDARY_QUALITY),
+            has_properties(sell_in=14, quality=21),
+            has_properties(sell_in=9, quality=gilded_rose.MAX_QUALITY),
+            has_properties(sell_in=4, quality=gilded_rose.MAX_QUALITY),
+            has_properties(sell_in=2, quality=4),
+        ))
+
+
+class TestItem:
+    @pytest.mark.parametrize('name,expected_class', [
+        pytest.param('+5 Dexterity Vest', gilded_rose.Item, id='regular_item'),
+        pytest.param(
+            gilded_rose.AGED_BRIE,
+            gilded_rose.QualityIncreasingItem,
+            id='item_that_increases_quality_with_age',
+        ),
+        pytest.param(gilded_rose.SULFURAS, gilded_rose.LegendaryItem, id='legendary_item'),
+        pytest.param(gilded_rose.BACKSTAGE_PASS, gilded_rose.BackstagePass, id='backstage_pass'),
+        pytest.param('Conjured Mana Cake', gilded_rose.ConjuredItem, id='conjured_item'),
+    ])
+    def test_creating_instances_of_proper_subclasses(self, name, expected_class):
+        item = gilded_rose.Item(name=name, sell_in=10, quality=20)
+
+        assert_that(type(item), is_(equal_to(expected_class)))
+
     @pytest.mark.parametrize('sell_in,quality,expected_sell_in,expected_quality', [
         pytest.param(10, 20, 9, 19, id='common_case'),
         pytest.param(1, 20, 0, 19, id='near_sell_by_date'),
         pytest.param(0, 20, -1, 18, id='sell_by_date_passed'),
-        pytest.param(10, MIN_QUALITY, 9, MIN_QUALITY, id='zero_quality'),
-        pytest.param(0, MIN_QUALITY, -1, MIN_QUALITY, id='zero_quality_and_sell_by_date_passed'),
-        pytest.param(0, 1, -1, MIN_QUALITY, id='quality_is_1_and_sell_by_date_passed'),
+        pytest.param(10, gilded_rose.MIN_QUALITY, 9, gilded_rose.MIN_QUALITY, id='zero_quality'),
+        pytest.param(
+            0,
+            gilded_rose.MIN_QUALITY,
+            -1,
+            gilded_rose.MIN_QUALITY,
+            id='zero_quality_and_sell_by_date_passed',
+        ),
+        pytest.param(0, 1, -1, gilded_rose.MIN_QUALITY, id='quality_is_1_and_sell_by_date_passed'),
     ])
-    def test_updates_regular_item(self, sell_in, quality, expected_sell_in, expected_quality):
-        items = [
-            Item(name='+5 Dexterity Vest', sell_in=sell_in, quality=quality),
-        ]
-        gilded_rose = GildedRose(items)
+    def test_item_update(self, sell_in, quality, expected_sell_in, expected_quality):
+        item = gilded_rose.Item(name='+5 Dexterity Vest', sell_in=sell_in, quality=quality)
 
-        gilded_rose.update_quality()
+        item.update()
 
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=expected_sell_in, quality=expected_quality),
-        ))
+        assert_that(item, has_properties(sell_in=expected_sell_in, quality=expected_quality))
 
+
+class TestQualityIncreasingItem:
     @pytest.mark.parametrize('sell_in,quality,expected_sell_in,expected_quality', [
         pytest.param(2, 1, 1, 2, id='common_case'),
         pytest.param(1, 1, 0, 2, id='near_sell_by_date'),
         pytest.param(0, 1, -1, 3, id='sell_by_date_passed'),
         pytest.param(10, 0, 9, 1, id='zero_quality'),
         pytest.param(0, 0, -1, 2, id='zero_quality_and_sell_by_date_passed'),
-        pytest.param(2, 50, 1, MAX_QUALITY, id='quality_is_50'),
-        pytest.param(0, 49, -1, MAX_QUALITY, id='quality_is_49_and_sell_by_date_passed'),
+        pytest.param(2, 50, 1, gilded_rose.MAX_QUALITY, id='quality_is_50'),
+        pytest.param(0, 49, -1, gilded_rose.MAX_QUALITY, id='quality_is_49_and_sell_by_date_passed'),
     ])
-    def test_updates_item_that_increases_quality_with_age(self, sell_in, quality, expected_sell_in, expected_quality):
-        items = [
-            Item(name=AGED_BRIE, sell_in=sell_in, quality=quality),
-        ]
-        gilded_rose = GildedRose(items)
+    def test_item_update(self, sell_in, quality, expected_sell_in, expected_quality):
+        item = gilded_rose.Item(name=gilded_rose.AGED_BRIE, sell_in=sell_in, quality=quality)
 
-        gilded_rose.update_quality()
+        item.update()
 
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=expected_sell_in, quality=expected_quality),
-        ))
+        assert_that(item, has_properties(sell_in=expected_sell_in, quality=expected_quality))
+
+
+class TestLegendaryItem:
+    def test_initialization_with_invalid_quality(self):
+        item = gilded_rose.Item(name=gilded_rose.SULFURAS, sell_in=0, quality=42)
+
+        assert_that(item.quality, is_(equal_to(gilded_rose.LEGENDARY_QUALITY)))
 
     @pytest.mark.parametrize('sell_in,quality,expected_sell_in,expected_quality', [
-        pytest.param(2, LEGENDARY_QUALITY, 2, LEGENDARY_QUALITY, id='common_case'),
-        pytest.param(0, LEGENDARY_QUALITY, 0, LEGENDARY_QUALITY, id='sell_by_date_passed'),
-        pytest.param(-1, LEGENDARY_QUALITY, -1, LEGENDARY_QUALITY, id='negative_sell_in'),
+        pytest.param(2, gilded_rose.LEGENDARY_QUALITY, 2, gilded_rose.LEGENDARY_QUALITY, id='common_case'),
+        pytest.param(0, gilded_rose.LEGENDARY_QUALITY, 0, gilded_rose.LEGENDARY_QUALITY, id='sell_by_date_passed'),
+        pytest.param(-1, gilded_rose.LEGENDARY_QUALITY, -1, gilded_rose.LEGENDARY_QUALITY, id='negative_sell_in'),
     ])
-    def test_does_not_update_legendary_item(self, sell_in, quality, expected_sell_in, expected_quality):
-        items = [
-            Item(name=SULFURAS, sell_in=sell_in, quality=quality),
-        ]
-        gilded_rose = GildedRose(items)
+    def test_item_update(self, sell_in, quality, expected_sell_in, expected_quality):
+        item = gilded_rose.Item(name=gilded_rose.SULFURAS, sell_in=sell_in, quality=quality)
 
-        gilded_rose.update_quality()
+        item.update()
 
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=expected_sell_in, quality=expected_quality),
-        ))
+        assert_that(item, has_properties(sell_in=expected_sell_in, quality=expected_quality))
 
+
+class TestBackstagePass:
     @pytest.mark.parametrize('sell_in,quality,expected_sell_in,expected_quality', [
         pytest.param(12, 1, 11, 2, id='common_case'),
-        pytest.param(12, 50, 11, MAX_QUALITY, id='quality_is_50'),
+        pytest.param(12, 50, 11, gilded_rose.MAX_QUALITY, id='quality_is_50'),
         pytest.param(10, 1, 9, 3, id='sell_in_is_10'),
-        pytest.param(10, 49, 9, MAX_QUALITY, id='sell_in_is_10_and_quality_is_49'),
+        pytest.param(10, 49, 9, gilded_rose.MAX_QUALITY, id='sell_in_is_10_and_quality_is_49'),
         pytest.param(5, 1, 4, 4, id='sell_in_is_5'),
-        pytest.param(5, 48, 4, MAX_QUALITY, id='sell_in_is_5_and_quality_is_48'),
+        pytest.param(5, 48, 4, gilded_rose.MAX_QUALITY, id='sell_in_is_5_and_quality_is_48'),
         pytest.param(1, 1, 0, 4, id='near_sell_by_date'),
-        pytest.param(0, 42, -1, MIN_QUALITY, id='sell_by_date_passed'),
-        pytest.param(-1, MIN_QUALITY, -2, MIN_QUALITY, id='negative_sell_in'),
+        pytest.param(0, 42, -1, gilded_rose.MIN_QUALITY, id='sell_by_date_passed'),
+        pytest.param(-1, gilded_rose.MIN_QUALITY, -2, gilded_rose.MIN_QUALITY, id='negative_sell_in'),
     ])
-    def test_backstage_passes(self, sell_in, quality, expected_sell_in, expected_quality):
-        items = [
-            Item(name=BACKSTAGE_PASS, sell_in=sell_in, quality=quality),
-        ]
-        gilded_rose = GildedRose(items)
+    def test_item_update(self, sell_in, quality, expected_sell_in, expected_quality):
+        item = gilded_rose.Item(name=gilded_rose.BACKSTAGE_PASS, sell_in=sell_in, quality=quality)
 
-        gilded_rose.update_quality()
+        item.update()
 
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=expected_sell_in, quality=expected_quality),
-        ))
+        assert_that(item, has_properties(sell_in=expected_sell_in, quality=expected_quality))
 
+
+class TestConjuredItem:
     @pytest.mark.parametrize('sell_in,quality,expected_sell_in,expected_quality', [
         pytest.param(10, 20, 9, 18, id='common_case'),
         pytest.param(0, 20, -1, 16, id='sell_by_date_passed'),
-        pytest.param(10, MIN_QUALITY, 9, MIN_QUALITY, id='zero_quality'),
-        pytest.param(0, MIN_QUALITY, -1, MIN_QUALITY, id='zero_quality_and_sell_by_date_passed'),
-        pytest.param(0, 3, -1, MIN_QUALITY, id='quality_is_3_and_sell_by_date_passed'),
+        pytest.param(10, gilded_rose.MIN_QUALITY, 9, gilded_rose.MIN_QUALITY, id='zero_quality'),
+        pytest.param(
+            0,
+            gilded_rose.MIN_QUALITY,
+            -1,
+            gilded_rose.MIN_QUALITY,
+            id='zero_quality_and_sell_by_date_passed',
+        ),
+        pytest.param(0, 3, -1, gilded_rose.MIN_QUALITY, id='quality_is_3_and_sell_by_date_passed'),
     ])
-    def test_conjured_items(self, sell_in, quality, expected_sell_in, expected_quality):
-        items = [
-            Item(name='Conjured Mana Cake', sell_in=sell_in, quality=quality),
-        ]
-        gilded_rose = GildedRose(items)
+    def test_item_update(self, sell_in, quality, expected_sell_in, expected_quality):
+        item = gilded_rose.Item(name='Conjured Mana Cake', sell_in=sell_in, quality=quality)
 
-        gilded_rose.update_quality()
+        item.update()
 
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=expected_sell_in, quality=expected_quality),
-        ))
-
-    def test_combined_case(self):
-        items = [
-            Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
-            Item(name=AGED_BRIE, sell_in=2, quality=MIN_QUALITY),
-            Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
-            Item(name=SULFURAS, sell_in=0, quality=LEGENDARY_QUALITY),
-            Item(name=SULFURAS, sell_in=-1, quality=LEGENDARY_QUALITY),
-            Item(name=BACKSTAGE_PASS, sell_in=15, quality=20),
-            Item(name=BACKSTAGE_PASS, sell_in=10, quality=49),
-            Item(name=BACKSTAGE_PASS, sell_in=5, quality=49),
-            Item(name='Conjured Mana Cake', sell_in=3, quality=6),
-        ]
-        gilded_rose = GildedRose(items)
-
-        gilded_rose.update_quality()
-
-        assert_that(gilded_rose.items, contains_exactly(
-            has_properties(sell_in=9, quality=19),
-            has_properties(sell_in=1, quality=1),
-            has_properties(sell_in=4, quality=6),
-            has_properties(sell_in=0, quality=LEGENDARY_QUALITY),
-            has_properties(sell_in=-1, quality=LEGENDARY_QUALITY),
-            has_properties(sell_in=14, quality=21),
-            has_properties(sell_in=9, quality=MAX_QUALITY),
-            has_properties(sell_in=4, quality=MAX_QUALITY),
-            has_properties(sell_in=2, quality=4),
-        ))
+        assert_that(item, has_properties(sell_in=expected_sell_in, quality=expected_quality))
